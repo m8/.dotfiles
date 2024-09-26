6159253ed55f2cab2fe559caf029f5712034d4b4
musa unal <musa.unal@epfl.ch>
2024-09-26 10:33:09 +0200

    changes

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -3,7 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade
 
 soft_packages=(
-
     gpick \             # Color picker
     keepassx \          # Password manager
     chromium-browser \  # Chromium
@@ -12,17 +11,14 @@ soft_packages=(
     vlc \               # Video player
     pandoc \            # Document converter
     gufw \              # Firewall
-    gnuplot \           # Plotting
-    grsync \            # Backup
-    gparted \           # Partition editor
     gnome-calculator \  # Calculator
     ncdu \              # Disk usage analyzer
     gvfs-backends \     # File transfer
-    gnome-clocks \       # Clock
 )
 
 dev_packages=(
-    gcc \
+	zsh \
+    	gcc \
     g++ \
     cmake \
     build-essential \
@@ -57,7 +53,6 @@ extensions=(
     ms-vscode.cpptools \
     GitHub.vscode-pull-request-github \
     ms-vscode-remote.vscode-remote-extensionpack \
-    James-Yu.latex-workshop \
 )
 
 code --install-extension ${extensions[@]}
@@ -66,17 +61,4 @@ code --install-extension ${extensions[@]}
 wget https://zoom.us/client/latest/zoom_amd64.deb -O /tmp/zoom_amd64.deb
 sudo apt install /tmp/zoom_amd64.deb
 
-# Install albertlauncher
-# sudo add-apt-repository ppa:nilarimogard/webupd8
-# sudo apt update
-echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_22.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
-curl -fsSL https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_22.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_manuelschneid3r.gpg > /dev/null
-sudo apt update
-sudo apt install albert
-
-
-# Add /src/scripts to PATH if it doesn't exist
-if [[ ! $PATH =~ .*\/src\/scripts.* ]]; then
-    echo "export PATH=$PATH:/src/scripts" >> ~/.bashrc
-fi
 
